102b39553d988d271e4d19150e55743dacfbd585
Tomas Menito <tomasmenito@gmail.com>
2021-08-21 19:59:05 -0300

    VerticalSequence generator implementation

--- magneto/sequence_generator.py
+++ magneto/sequence_generator.py
@@ -12,3 +12,14 @@ class HorizontalSequenceGenerator(SequenceGenerator):
     def generate_sequences(self, matrix: Sequence[Sequence[str]]) -> Iterable[Iterable[str]]:
         for row in matrix:
             yield row
+
+
+class VerticalSequenceGenerator(SequenceGenerator):
+    def generate_sequences(self, matrix: Sequence[Sequence[str]]) -> Iterable[Iterable[str]]:
+        length = len(matrix[0])
+        for index in range(length):
+            yield self._vertical_sequence(matrix, index)
+
+    def _vertical_sequence(self, matrix: Sequence[Sequence[str]], index: int) -> Iterable[str]:
+        for row in matrix:
+            yield row[index]
--- tests/test_sequence_generator.py
+++ tests/test_sequence_generator.py
@@ -1,6 +1,6 @@
 import pytest
 
-from magneto.sequence_generator import HorizontalSequenceGenerator
+from magneto.sequence_generator import HorizontalSequenceGenerator, VerticalSequenceGenerator
 
 
 class TestHorizontalSequenceGenerator:
@@ -15,3 +15,17 @@ class TestHorizontalSequenceGenerator:
     def test_horizontal_sequence_generator(self, matrix, expected):
         sequences = list(HorizontalSequenceGenerator().generate_sequences(matrix))
         assert sequences == expected
+
+
+class TestVerticalSequenceGenerator:
+    @pytest.mark.parametrize(
+        "matrix, expected",
+        [
+            (["aa"], ["a", "a"]),
+            (["aa", "bb"], ["ab", "ab"]),
+            (["ab", "cd"], ["ac", "bd"]),
+        ],
+    )
+    def test_vertical_sequence_generator(self, matrix, expected):
+        sequences = list(VerticalSequenceGenerator().generate_sequences(matrix))
+        assert ["".join(s) for s in sequences] == expected
