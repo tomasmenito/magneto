d467136e3372ae73a1822c5d398a0705be6129a1
Tomas Menito <tomasmenito@gmail.com>
2021-08-22 07:42:02 -0300

    Implement ObliqueSequenceGenerator

--- magneto/sequence_generator.py
+++ magneto/sequence_generator.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 from typing import Iterable, Sequence
 
 
@@ -23,3 +24,46 @@ class VerticalSequenceGenerator(SequenceGenerator):
     def _vertical_sequence(self, matrix: Sequence[Sequence[str]], index: int) -> Iterable[str]:
         for row in matrix:
             yield row[index]
+
+
+class ObliqueSequenceGenerator(SequenceGenerator):
+    class Direction(Enum):
+        DOWN_RIGHT = 1
+        DOWN_LEFT = 2
+
+    def __init__(self, direction: Direction = Direction.DOWN_RIGHT):
+        self.direction = direction
+
+    def generate_sequences(self, matrix: Sequence[Sequence[str]]) -> Iterable[Iterable[str]]:
+        height = len(matrix)
+        width = len(matrix[0])
+        if self.direction == self.Direction.DOWN_RIGHT:
+            for row in reversed(range(height)):
+                yield self._oblique_sequence(matrix, row, 0)
+            for column in range(1, width):
+                yield self._oblique_sequence(matrix, 0, column)
+        elif self.direction == self.direction.DOWN_LEFT:
+            for row in reversed(range(height)):
+                yield self._oblique_sequence(matrix, row, width - 1)
+            for column in reversed(range(0, width - 1)):
+                yield self._oblique_sequence(matrix, 0, column)
+        else:
+            raise NotImplementedError()
+
+    def _oblique_sequence(self, matrix: Sequence[Sequence[str]], row: int, column: int) -> Iterable[str]:
+        if self.direction == self.Direction.DOWN_RIGHT:
+            increment = 1
+        elif self.direction == self.direction.DOWN_LEFT:
+            increment = -1
+        else:
+            raise NotImplementedError()
+        while True:
+            try:
+                yield matrix[row][column]
+            except IndexError:
+                break
+            else:
+                row += 1
+                column += increment
+                if column < 0:
+                    break
--- tests/test_sequence_generator.py
+++ tests/test_sequence_generator.py
@@ -1,6 +1,10 @@
 import pytest
 
-from magneto.sequence_generator import HorizontalSequenceGenerator, VerticalSequenceGenerator
+from magneto.sequence_generator import (
+    HorizontalSequenceGenerator,
+    ObliqueSequenceGenerator,
+    VerticalSequenceGenerator,
+)
 
 
 class TestHorizontalSequenceGenerator:
@@ -28,4 +32,38 @@ class TestVerticalSequenceGenerator:
     )
     def test_vertical_sequence_generator(self, matrix, expected):
         sequences = list(VerticalSequenceGenerator().generate_sequences(matrix))
+        assert ["".join(s) for s in sequences] == expected
+
+
+class TestObliqueSequenceGenerator:
+    @pytest.mark.parametrize(
+        "matrix, expected",
+        [
+            (["aa"], ["a", "a"]),
+            (["aa", "bb"], ["b", "ab", "a"]),
+            (["ab", "cd"], ["c", "ad", "b"]),
+        ],
+    )
+    def test_oblique_sequence_generator_down_right(self, matrix, expected):
+        sequences = list(
+            ObliqueSequenceGenerator(
+                direction=ObliqueSequenceGenerator.Direction.DOWN_RIGHT
+            ).generate_sequences(matrix)
+        )
+        assert ["".join(s) for s in sequences] == expected
+
+    @pytest.mark.parametrize(
+        "matrix, expected",
+        [
+            (["aa"], ["a", "a"]),
+            (["aa", "bb"], ["b", "ab", "a"]),
+            (["ab", "cd"], ["d", "bc", "a"]),
+        ],
+    )
+    def test_oblique_sequence_generator_down_left(self, matrix, expected):
+        sequences = list(
+            ObliqueSequenceGenerator(
+                direction=ObliqueSequenceGenerator.Direction.DOWN_LEFT
+            ).generate_sequences(matrix)
+        )
         assert ["".join(s) for s in sequences] == expected
