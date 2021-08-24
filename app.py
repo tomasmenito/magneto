3b595d25203ee4a5875d3ef77c5d906506ae1c00
Tomas Menito <tomasmenito@gmail.com>
2021-08-23 22:03:20 -0300

    Database implementation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response, request
 
+from database import DynamoDBDatabase
 from magneto.mutant_identifier import SimpleMutantIdentifier
 from magneto.sequence_generator import AllDirectionsSequenceGenerator
 from magneto.sequence_identifier import SimpleSequenceIdentifier
@@ -8,8 +9,17 @@ app = Flask(__name__)
 
 identifier = SimpleMutantIdentifier(AllDirectionsSequenceGenerator(), SimpleSequenceIdentifier())
 
+database = DynamoDBDatabase()
+
 
 @app.route("/mutant", methods=["POST"])
 def mutant():
-    is_mutant = identifier.is_mutant(request.json["dna"])
+    dna_row = request.json["dna"]
+    is_mutant = identifier.is_mutant(dna_row)
+    database.create_record(dna_row, is_mutant)
     return Response(status=200 if is_mutant else 403)
+
+
+@app.route("/stats", methods=["GET"])
+def stats():
+    return database.stats()
---
+++ database.py
@@ -0,0 +1,43 @@
+import json
+from abc import ABC, abstractmethod
+from numbers import Number
+from typing import Sequence
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+
+class Database(ABC):
+    @abstractmethod
+    def create_record(self, dna_row: Sequence[Sequence[str]], is_mutant: bool):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def stats(self) -> dict[str, Number]:
+        raise NotImplementedError()
+
+
+class DynamoDBDatabase:
+    def __init__(self, table_name: str = "mutant"):
+        self.table_name = table_name
+        self.table = boto3.resource("dynamodb").Table(self.table_name)
+
+    def create_record(self, dna_row: Sequence[Sequence[str]], is_mutant: bool):
+        return self.table.put_item(Item={"is_mutant": int(is_mutant), "dna_rows": json.dumps(dna_row)})
+
+    def stats(self) -> dict[str, Number]:
+        stats = {
+            "count_mutant_dna": self.mutant_count(True),
+            "count_human_dna": self.mutant_count(False),
+        }
+        try:
+            ratio: float = stats["count_mutant_dna"] / stats["count_human_dna"]
+        except ZeroDivisionError:
+            ratio = 0.0
+        stats["ratio"] = ratio
+        return stats
+
+    def mutant_count(self, is_mutant: bool) -> int:
+        return self.table.query(Select="COUNT", KeyConditionExpression=Key("is_mutant").eq(int(is_mutant)))[
+            "Count"
+        ]
